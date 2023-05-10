e82b770596523120caecc3dc2fdba422d6a0b8fc
Evan <evanknight18@gmail.com>
2023-05-09 18:31:14 -0600

    server.js edits

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uniqid = require('uniqid');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+const util = require('util');
 
 // Middleware
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+const readFromFile = util.promisify(fs.readFile);
 
 // HTML routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 // API routes
 app.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-  res.json(notes);
+  // const notes = JSON.parse(fs.readFileAsync('db/db.json', 'utf8'));
+  // console.log(notes);
+  // let parsedNotes = [].concat(notes);
+  // console.log(parsedNotes);
+  // res.json(parsedNotes);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-  newNote.id = uniqid();
+  newNote.id = Date.now().toString();
   const notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
   notes.push(newNote);
   fs.writeFileSync('db/db.json', JSON.stringify(notes));
@@ -41,4 +46,4 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
